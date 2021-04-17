1a6c542754468099e5c93780ccd90562319401e8
Aleksander Kuźma <aleksander.a.kuzma@gmail.com>
2021-04-17 12:52:03 +0200

    feat(@web/app): add @web/navigation

--- apps/web/app/src/App.tsx
+++ apps/web/app/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { ThemeProvider } from '@shared/theme';
+import { Navigation } from '@web/navigation';
 import { Routes } from '@web/routes';
+import './App.css';
 
 export const App = () => (
     <ThemeProvider>
+        <Navigation />
         <Routes />
     </ThemeProvider>
 );
