332c7579c381659868c0cc8ac9e5738a2fa0305e
Aleksander Kuźma <aleksander.a.kuzma@gmail.com>
2021-04-17 12:50:06 +0200

    feat(@shared/theme): add dark and light theme

--- shared/packages/theme/src/Theme.tsx
+++ shared/packages/theme/src/Theme.tsx
@@ -1,11 +1,52 @@
-import React, { createContext, FunctionComponent, useContext } from 'react';
+import React, { createContext, FunctionComponent, useEffect, useState } from 'react';
 
-const ThemeContext = createContext({ red: '#ff0000', green: '#00ff00', blue: '#0000ff' });
+const themes = {
+    dark: {
+        color: '#A3B4C8',
+        'highlight-color-1': '#CC6D3D',
+        'highlight-color-2': '#B74803',
+        'background-color': '#022E51',
+    },
+    light: {
+        color: '#285185',
+        'highlight-color-1': '#D67940',
+        'highlight-color-2': '#6F4849',
+        'background-color': '#CCD9E2',
+    },
+};
 
-export const useTheme = () => useContext(ThemeContext);
+type Theme = keyof typeof themes;
+type Variables = typeof themes.dark & typeof themes.light;
 
-export const ThemeProvider: FunctionComponent = ({ children }) => (
-    <ThemeContext.Provider value={{ red: '#ff0000', green: '#00ff00', blue: '#0000ff' }}>
-        {children}
-    </ThemeContext.Provider>
-);
+export const ThemeSelectorContext = createContext({
+    themeName: 'dark',
+    toggleTheme: () => {},
+});
+
+const setCSSVariables = (theme: Variables) => {
+    Object.entries(theme).forEach(([variableName, variable]) => {
+        document.documentElement.style.setProperty(`--${variableName}`, variable);
+    });
+};
+
+export const ThemeProvider: FunctionComponent = ({ children }) => {
+    const [themeName, setThemeName] = useState<Theme>('dark');
+    const [theme, setTheme] = useState(themes[themeName]);
+
+    useEffect(() => {
+        console.log(theme);
+        setCSSVariables(theme);
+    }, [theme]);
+
+    const toggleTheme = () => {
+        if (theme === themes.dark) {
+            setTheme(themes.light);
+            setThemeName('light');
+        } else {
+            setTheme(themes.dark);
+            setThemeName('dark');
+        }
+    };
+
+    return <ThemeSelectorContext.Provider value={{ toggleTheme, themeName }}>{children}</ThemeSelectorContext.Provider>;
+};
